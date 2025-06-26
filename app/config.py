6844ea23f23c5fc9871f9d4c9423c318d9182236
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2025-06-26 10:05:36 +0700

    defien orgin

--- app/config.py
+++ app/config.py
@@ -1,9 +1,6 @@
 import os
 
 class Settings:
-    # A comma-separated list of allowed origins for CORS.
-    # Your main application's URL should be in this list.
-    # Example: "http://localhost:5000,https://my-frontend.com"
-    ALLOWED_ORIGINS: list[str] = os.environ.get("ALLOWED_ORIGINS", "http://localhost:5000,http://127.0.0.1:5000").split(",")
+    ALLOWED_ORIGINS: list[str] = os.environ.get("ALLOWED_ORIGINS", "http://localhost:5000,https://apparent-nadeen-aupp-54d2fac0.koyeb.app").split(",")
 
 settings = Settings()
